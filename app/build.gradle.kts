dd8ed1d95d0136d4285f1f55a90dfa82fe78d2c1
Ben Piatt <piattb4@gmail.com>
2025-08-28 14:36:44 -0500

    Added Kotlin JVM plugin, added relevant modules to app module as dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
@@ -44,14 +43,15 @@ android {
     }
 }
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(project(":core:common"))
+    implementation(project(":core:navigation"))
+    implementation(project(":core:network"))
+    implementation(project(":core:ui"))
+    implementation(project(":auth:ui"))
+    implementation(project(":appointments:ui"))
+    implementation(project(":chat:ui"))
+    implementation(project(":journal:ui"))
+    implementation(project(":profile:ui"))
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
